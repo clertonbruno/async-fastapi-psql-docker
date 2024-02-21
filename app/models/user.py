a56d6e146948e3d43f28cb05175b7db894ec01d3
Clerton <clerton.sampaio@druid.com.br>
2024-02-20 22:59:56 -0300

    Add User model and related classes

---
+++ app/models/user.py
@@ -0,0 +1,29 @@
+from models.base import IdMixin, TimestampMixin
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
+
+class UserBase(SQLModel):
+    first_name: str = None
+    last_name: str = None
+    email: EmailStr = Field(
+        nullable=False, index=True, sa_column_kwargs={"unique": True}
+    )
+    is_active: bool = True
+
+
+class UserCreate(UserBase): ...
+
+
+class UserUpdate(UserBase):
+    first_name: str = None
+    last_name: str = None
+    email: EmailStr = None
+    is_active: bool = None
+
+
+class User(IdMixin, TimestampMixin, UserBase, table=True):
+    __tablename__ = "users"
+
+
+class UserResponse(User, table=False): ...
