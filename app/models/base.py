1569b01383a44258a3dd7b46644fe741926f03da
Clerton <clerton.sampaio@druid.com.br>
2024-02-20 22:34:01 -0300

    Add IdMixin and TimestampMixin to base.py

---
+++ app/models/base.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlmodel import Field, SQLModel
+from uuid_extensions import uuid7
+
+
+class IdMixin(SQLModel):
+    id: UUID = Field(
+        default_factory=uuid7,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+
+
+class TimestampMixin(SQLModel):
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
