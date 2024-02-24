f27d84579d00582640f52f973a453809c73456ad
Clerton <clerton.sampaio@druid.com.br>
2024-02-24 16:30:35 -0300

    Add CRUD tests for user module

--- app/tests/crud/user.py
+++ app/tests/crud/user.py
@@ -34,3 +34,50 @@ async def test_get_user(session: AsyncSession):
 async def test_get_nonexistent_user(session: AsyncSession):
     fetched_user = await get_user(session, "thisemaildoesntexist@example.com")
     assert fetched_user is None
+
+
+async def test_get_user_by_email(session: AsyncSession):
+    user = UserCreate(email="test@example.com")
+    created_user = await create_user(session, user)
+    fetched_user = await get_user_by_email(session, user.email)
+    assert fetched_user == created_user
+
+
+async def test_get_nonexisting_user_by_email(session: AsyncSession):
+    fetched_user = await get_user_by_email(session, "nonexistent@example.com")
+    assert fetched_user is None
+
+
+async def test_update_user(session: AsyncSession):
+    user = UserCreate(
+        first_name="John",
+        last_name="Doe",
+        email="test@example.com",
+    )
+    created_user = await create_user(session, user)
+    updated_user = await update_user(session, user, UserUpdate(first_name="Maverick"))
+    assert updated_user.id == created_user.id
+    assert updated_user.email == created_user.email
+    assert updated_user.first_name != created_user.first_name
+
+
+async def test_update_nonexistent_user(session: AsyncSession):
+    try:
+        await update_user(session, uuid7(), UserUpdate(first_name="John"))
+    except HTTPException as e:
+        assert e.status_code == 404
+        assert e.detail == "User not found"
+
+
+async def test_delete_user(session: AsyncSession):
+    user = UserCreate(email="test@example.com")
+    created_user = await create_user(session, user)
+    deleted_count = await delete_user(session, created_user.id)
+    assert deleted_count == 1
+    fetched_user = await get_user(session, created_user.id)
+    assert fetched_user is None
+
+
+async def test_delete_nonexistent_user(session: AsyncSession):
+    deleted_count = await delete_user(session, uuid7())
+    assert deleted_count == 0
